514fa1f86eb473d7fea40c4fb567953942cce941
ZsembesTorpe <molnar.mate2003@gmail.com>
2025-05-17 11:04:18 +0200

    SZTE Mobilalkalmazás fejlesztés I.

--- app/src/main/java/com/example/bioshop/ShopListActivity.java
+++ app/src/main/java/com/example/bioshop/ShopListActivity.java
@@ -1,11 +1,15 @@
 package com.example.bioshop;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.util.Log;
@@ -38,141 +42,94 @@ import java.util.ArrayList;
 
 public class ShopListActivity extends AppCompatActivity {
     private static final String LOG_TAG = ShopListActivity.class.getName();
+    private static final String PREF_KEY = MainActivity.class.getPackage().toString();
     private FirebaseUser user;
+
     private FrameLayout redCircle;
     private TextView countTextView;
-    private int gridNumber=1;
     private int cartItems = 0;
-    private int queryLimit=8;
-    private RecyclerView mRecView;
+    private int gridNumber = 1;
+
+    // Member variables.
+    private RecyclerView mRecyclerView;
     private ArrayList<ShoppingItem> mItemsData;
     private ShoppingItemAdapter mAdapter;
-    private TextView contentTextView;
-    private NotificationHandler mNotH;
-    private FirebaseFirestore mFirestore;
-    private CollectionReference mItems;
-    boolean viewRow= true;
-    private AlarmManager mAlaMana;
 
 
-    private boolean shouldUpdateCart = false;
+
+    private SharedPreferences preferences;
 
 
+    private boolean viewRow = true;
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_shop_list);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        user= FirebaseAuth.getInstance().getCurrentUser();
 
-        if (user!=null){
-            Log.i(LOG_TAG,"Bejelentett felhasználó");
-
-        }else{
-            Log.i(LOG_TAG,"NEM bejelentett felhasználó");
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null) {
+            Log.d(LOG_TAG, "Authenticated user!");
+        } else {
+            Log.d(LOG_TAG, "Unauthenticated user!");
             finish();
-            overridePendingTransition(R.anim.animate_fade_enter,R.anim.animate_fade_exit);
         }
 
+/*        preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
+        if(preferences != null) {
+            cartItems = preferences.getInt("cartItems", 0);
+            gridNumber = preferences.getInt("gridNum", 1);
+        }*/
 
-        mRecView=findViewById(R.id.recyclerView);
-        mRecView.setLayoutManager(new GridLayoutManager(this,gridNumber));
-        mItemsData=new ArrayList<>();
-        mAdapter=new ShoppingItemAdapter(this,mItemsData);
-        mRecView.setAdapter(mAdapter);
-
-
-        mFirestore=FirebaseFirestore.getInstance();
-        mItems=mFirestore.collection("Items");
-
-        queryData();
-
-
-
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(Intent.ACTION_POWER_CONNECTED);
-        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
-        this.registerReceiver(powerReceiver,filter);
-
-        mNotH=new NotificationHandler(this);
-        mAlaMana=(AlarmManager) getSystemService(ALARM_SERVICE);
-        setAlarmManager();
+        // recycle view
+        mRecyclerView = findViewById(R.id.recyclerView);
+        // Set the Layout Manager.
+        mRecyclerView.setLayoutManager(new GridLayoutManager(
+                this, gridNumber));
+        // Initialize the ArrayList that will contain the data.
+        mItemsData = new ArrayList<>();
+        // Initialize the adapter and set it to the RecyclerView.
+        mAdapter = new ShoppingItemAdapter(this, mItemsData);
+        mRecyclerView.setAdapter(mAdapter);
+        // Get the data.
+        initializeData();
     }
-    BroadcastReceiver powerReceiver=new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action=intent.getAction();
-            if (action==null){
-                return;
-            }
-            switch (action){
-                case Intent.ACTION_POWER_CONNECTED:
-                    queryLimit=8;
-                    queryData();
-                    break;
-                case Intent.ACTION_POWER_DISCONNECTED:
-                    queryLimit=4;
-                    queryData();
-                    break;
-            }
 
-        }
-    };
     private void initializeData() {
-        String[] itemList = getResources().getStringArray(R.array.termeknevek);
-        String[] iteminfo= getResources().getStringArray(R.array.termekleirasok);
-        String[] itemPrice= getResources().getStringArray(R.array.arak);
-        TypedArray itemImage= getResources().obtainTypedArray(R.array.termekkepek);
-        TypedArray itemsRate= getResources().obtainTypedArray(R.array.ertekelesek);
+        // Get the resources from the XML file.
+        String[] itemsList = getResources()
+                .getStringArray(R.array.termeknevek);
+        String[] itemsInfo = getResources()
+                .getStringArray(R.array.termekleirasok);
+        String[] itemsPrice = getResources()
+                .getStringArray(R.array.arak);
+        TypedArray itemsImageResources =
+                getResources().obtainTypedArray(R.array.termekkepek);
+        TypedArray itemRate = getResources().obtainTypedArray(R.array.ertekelesek);
 
-
-        for (int i = 0; i < itemList.length; i++) {
-            mItems.add(new ShoppingItem(
-                    itemList[i],
-                    iteminfo[i],
-                    itemPrice[i],
-                    itemsRate.getFloat(i,0),
-                    itemImage.getResourceId(i,0)));
-        }
-
-        itemImage.recycle();
-
-    }
-    private void queryData() {
-
+        // Clear the existing data (to avoid duplication).
         mItemsData.clear();
 
+        // Create the ArrayList of Sports objects with the titles and
+        // information about each sport.
+        for (int i = 0; i < itemsList.length; i++) {
+            mItemsData.add(new ShoppingItem(itemsList[i], itemsInfo[i], itemsPrice[i], itemRate.getFloat(i, 0),
+                    itemsImageResources.getResourceId(i, 0)));
+        }
 
-        mItems.orderBy("name").limit(queryLimit).get().addOnSuccessListener(queryDocumentSnapshots -> {
+        // Recycle the typed array.
+        itemsImageResources.recycle();
 
-            for (QueryDocumentSnapshot document:queryDocumentSnapshots) {
-                ShoppingItem item = document.toObject(ShoppingItem.class);
-                mItemsData.add(item);
-
-
-            }
-            if (mItemsData.size()==0){
-                initializeData();
-                queryData();
-            }
-            mAdapter.notifyDataSetChanged();
-        });
-
-
+        // Notify the adapter of the change.
+        mAdapter.notifyDataSetChanged();
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.shop_list_menu, menu);
         MenuItem menuItem = menu.findItem(R.id.search_bar);
         SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
-        super.onCreateOptionsMenu(menu);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
@@ -184,119 +141,83 @@ public class ShopListActivity extends AppCompatActivity {
                 Log.d(LOG_TAG, s);
                 mAdapter.getFilter().filter(s);
                 return false;
-
             }
         });
-
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.logout:
+                Log.d(LOG_TAG, "Logout clicked!");
                 FirebaseAuth.getInstance().signOut();
                 finish();
                 return true;
             case R.id.settings:
-                Log.d(LOG_TAG,"beállítások megnyomva");
+                Log.d(LOG_TAG, "Setting clicked!");
+                FirebaseAuth.getInstance().signOut();
+                finish();
                 return true;
             case R.id.cart:
-                Log.d(LOG_TAG,"kosár megnyomva");
+                Log.d(LOG_TAG, "Cart clicked!");
                 return true;
             case R.id.view_selector:
-                Log.d(LOG_TAG,"nézetváltás megnyomva");
-                if (viewRow){
-                    changeSpanCount(item,R.drawable.baseline_grid_view_24,1);
-                }else {
-                    changeSpanCount(item,R.drawable.grid_row,3);
+                if (viewRow) {
+                    changeSpanCount(item, R.drawable.baseline_grid_view_24, 1);
+                } else {
+                    changeSpanCount(item, R.drawable.grid_row, 3);
                 }
                 return true;
-
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    private void changeSpanCount(MenuItem item, int drawableID, int spanCount) {
+    private void changeSpanCount(MenuItem item, int drawableId, int spanCount) {
         viewRow = !viewRow;
-        item.setIcon(drawableID);
-        GridLayoutManager layoutManager= (GridLayoutManager) mRecView.getLayoutManager();
+        item.setIcon(drawableId);
+        GridLayoutManager layoutManager = (GridLayoutManager) mRecyclerView.getLayoutManager();
         layoutManager.setSpanCount(spanCount);
-
     }
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
-        final MenuItem alertMenuItem=menu.findItem(R.id.cart);
-        FrameLayout rootView=(FrameLayout) alertMenuItem.getActionView();
+        final MenuItem alertMenuItem = menu.findItem(R.id.cart);
+        FrameLayout rootView = (FrameLayout) alertMenuItem.getActionView();
+
         redCircle = (FrameLayout) rootView.findViewById(R.id.view_alert_red_circle);
         countTextView = (TextView) rootView.findViewById(R.id.view_alert_count_textview);
 
-        rootView.setOnClickListener(v -> onOptionsItemSelected(alertMenuItem));
-
-      if (shouldUpdateCart && countTextView != null && redCircle != null) {
-            if (cartItems > 0) {
-                countTextView.setText(String.valueOf(cartItems));
-                redCircle.setVisibility(View.VISIBLE);
-            } else {
-                countTextView.setText("");
-                redCircle.setVisibility(View.GONE);
+        rootView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOptionsItemSelected(alertMenuItem);
             }
-            shouldUpdateCart = false;
-        }
-
+        });
         return super.onPrepareOptionsMenu(menu);
     }
-    public void updateAlertIcon(ShoppingItem item) {
-    /*    if (item == null) {
 
-            cartItems = 0;
-            countTextView.setText("");
-            redCircle.setVisibility(View.GONE);
-        }
-*/
+    public void updateAlertIcon() {
         cartItems = (cartItems + 1);
-        if (0<cartItems) {
+        if (0 < cartItems) {
             countTextView.setText(String.valueOf(cartItems));
         } else {
             countTextView.setText("");
         }
-        redCircle.setVisibility((cartItems > 0) ? View.VISIBLE : View.GONE);
-      /*  mItems.document(item._getId()).update("cartedCount", item.getCartedCount() + 1)
-                .addOnFailureListener(fail -> {
-                    Toast.makeText(this, "Item " + item._getId() + " cannot be changed.", Toast.LENGTH_LONG).show();
-                });
 
-    mNotH.send(item.getName());*/
-
+        redCircle.setVisibility((cartItems > 0) ? VISIBLE : GONE);
     }
- /*  public void deleteItem(ShoppingItem item){
-        DocumentReference ref=mItems.document(item._getId());
-        ref.delete()
-                .addOnSuccessListener(success -> {
-                    Log.d(LOG_TAG, "Item is successfully deleted: " + item._getId());
-                })
-                .addOnFailureListener(fail -> {
-                    Toast.makeText(this, "Item " + item._getId() + " cannot be deleted.", Toast.LENGTH_LONG).show();
-                });
 
-        mNotH.cancel();
+/*    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("cartItems", cartItems);
+        editor.putInt("gridNum", gridNumber);
+        editor.apply();
+
+        Log.i(LOG_TAG, "onPause");
     }*/
-
-
-
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(powerReceiver);
-
-    }
-    private void setAlarmManager(){
-        Intent intent = new Intent(this, AlarmReceiver.class);
-        PendingIntent pendingIntent=PendingIntent.getBroadcast(this,0,intent,PendingIntent.FLAG_IMMUTABLE);
-      //  mAlaMana.set
-    }
-
 }
--- app/src/main/java/com/example/bioshop/ShoppingItemAdapter.java
+++ app/src/main/java/com/example/bioshop/ShoppingItemAdapter.java
@@ -1,8 +1,6 @@
 package com.example.bioshop;
 
-import android.content.ContentUris;
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,19 +11,13 @@ import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.example.bioshop.CartManager;
-import com.example.bioshop.R;
-import com.example.bioshop.ShopListActivity;
-import com.example.bioshop.ShoppingItem;
 
 import java.util.ArrayList;
-import java.util.Locale;
 
 public class ShoppingItemAdapter extends RecyclerView.Adapter<ShoppingItemAdapter.ViewHolder> implements Filterable {
     private ArrayList<ShoppingItem> mShoppingItemData;
@@ -122,7 +114,12 @@ public class ShoppingItemAdapter extends RecyclerView.Adapter<ShoppingItemAdapte
             mItemImage = itemView.findViewById(R.id.itemImage);
             mRatingBar = itemView.findViewById(R.id.ratingBar);
 
-
+            itemView.findViewById(R.id.add_to_kosar).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ((ShopListActivity)mContext).updateAlertIcon();
+                }
+            });
         }
 
         public void bindTo(ShoppingItem currentItem) {
@@ -133,14 +130,7 @@ public class ShoppingItemAdapter extends RecyclerView.Adapter<ShoppingItemAdapte
 
             Glide.with(mContext).load(currentItem.getImageResource()).into(mItemImage);
 
-          /*  View addToCartBtn = itemView.findViewById(R.id.add_to_kosar);
-            View deleteBtn = itemView.findViewById(R.id.delete);
 
-
-            if(mContext!=null){
-                addToCartBtn.setOnClickListener(view -> ((ShopListActivity) mContext).updateAlertIcon(currentItem));}
-                if(mContext!=null){
-                deleteBtn.setOnClickListener(view -> ((ShopListActivity) mContext).deleteItem(currentItem));}*/
             }
         }
     }
